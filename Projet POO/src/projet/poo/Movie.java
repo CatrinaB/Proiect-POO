4bf63ce466e05df5f4444f0a38d641d98c5cccca
MariaBrob <mariabroboneayahoo.com>
2017-12-28 21:25:13 +0200

    Am pus un nume

--- Projet POO/src/projet/poo/Movie.java
+++ Projet POO/src/projet/poo/Movie.java
@@ -4,14 +4,37 @@ import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 public class Movie {
+    private String name;
     private ArrayList<Ticket> tickets;
     private Room room;
     private GregorianCalendar dateTime;
+    
+    public Movie() {
+        
+    }
 
-    public Movie(ArrayList<Ticket> tickets, Room room, GregorianCalendar dateTime) {
+    public Movie(String name, Room room) {
+        this.name = name;
+        this.room = room;
+    }
+    
+    public Movie(String name, ArrayList<Ticket> tickets, Room room, GregorianCalendar dateTime) {
+        this.name = name;
         this.tickets = tickets;
         this.room = room;
         this.dateTime = dateTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 
     public ArrayList<Ticket> getTickets() {
@@ -37,6 +60,11 @@ public class Movie {
     public void setDateTime(GregorianCalendar dateTime) {
         this.dateTime = dateTime;
     }
+
+    @Override
+    public String toString() {
+        return "Movie{" + "name=" + name + ", tickets=" + tickets + ", room=" + room + ", dateTime=" + dateTime + '}';
+    }
     
     public void addTicket(Client client){
         ArrayList<Ticket> temp = client.getTickets();
@@ -48,3 +76,4 @@ public class Movie {
                 tickets.add(temp.get(i));
     }
 }
+
